7248bf06ab277184c3e31c3ab2965e78ed6a06dc
gnostertag <ostertag2000@gmail.com>
2023-05-02 11:03:25 -0700

    api endpoints

--- spec/requests/cows_spec.rb
+++ spec/requests/cows_spec.rb
@@ -2,6 +2,41 @@ require 'rails_helper'
 
 RSpec.describe "Cows", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of cows" do
+      Cow.create(
+        name: "Ferdinand",
+        age: 13,
+        enjoys: "chasing butterflies",
+        color: "black",
+        species: "angus",
+        image: "https://pixabay.com/photos/bull-field-beefmaster-summer-sunny-5866514/"
+      )
+      get '/cows'
+
+      cow = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(cow.length).to eq 1
+    end
   end
+  describe "POST /create" do
+    it "creates a cow" do
+      cow_params = {
+        cow: {
+        name: "Ferdinand",
+        age: 13,
+        enjoys: "chasing butterflies",
+        color: "black",
+        species: "angus",
+        image: "https://pixabay.com/photos/bull-field-beefmaster-summer-sunny-5866514/"
+        }
+      }
+      post '/cows', params: cow_params
+      expect(response).to have_http_status(200)
+
+      cow = Cow.first
+      expect(cow.name).to eq "Ferdinand"
+    end
+  end
+  
 end
+
